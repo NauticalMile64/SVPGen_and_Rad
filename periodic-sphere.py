ee9e2348ff14092c4e4a95141bf7f16029fa777e
NauticalMile64 <nolan.dyck20@gmail.com>
2021-06-17 10:05:38 -0400

    Remove MIT license text from files

--- periodic-sphere.py
+++ periodic-sphere.py
@@ -1,13 +1,4 @@
 '''
-The MIT License (MIT)
-Copyright (c) 2016 Nolan Dyck
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 This code has been run successfully on Ubuntu 14.10 with yadedaily version 1.20.0-72-bbff50f~trusty
 
 More recently tested with yade 2020.01a on Ubuntu 20.04.2 LTS with python 3.8.5
